6aaf9c282b25c14b323edab5c82da460c2eedcec
kyle-piddington <kpiddy@gmail.com>
2014-08-26 23:27:34 -0700

    Added basic SLERP'd look logic, will work when aligned to arbitrary plane

---
+++ Project/Assets/script/player/MoveToPlatform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToPlatform : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 screenpoint = Input.mousePosition;
+			Debug.DrawRay(transform.position, Camera.main.ScreenToWorldPoint(screenpoint));
+			CastRayToPlatform(Camera.main.ScreenPointToRay(screenpoint));
+		}
+	}
+
+	void CastRayToPlatform(Ray ray){
+		RaycastHit info;
+		if (Physics.Raycast (ray, out info)) {
+			Debug.DrawRay(info.transform.position,info.normal);
+		}
+
+	}
+
+}
---
+++ Project/Assets/script/player/PitchYawCamera.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PitchYawCamera : MonoBehaviour {
+	
+	private Vector3 camEuler = Vector3.zero;
+	private Vector3 lastMouse = Vector3.zero;
+
+	private Quaternion playerTarg;
+	private Quaternion camTarg;
+
+	private float camRotAgl;
+	// Use this for initialization
+	void Start () {
+		lastMouse = Input.mousePosition;
+		playerTarg = transform.rotation;
+		camTarg = Quaternion.identity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mouseDelta = getMouseDelta();
+		camEuler.y = -mouseDelta.x;
+		camRotAgl += mouseDelta.y;
+		Debug.Log(camRotAgl);
+		Quaternion plyrRot = Quaternion.Euler (new Vector3(0,camEuler.y,0));
+		playerTarg = playerTarg * plyrRot;
+
+		camRotAgl = Mathf.Clamp (camRotAgl, -60, 60);
+		Quaternion camRot = Quaternion.Euler(camRotAgl,0,0);
+		Transform cam = transform.GetChild(0).transform;
+
+		camTarg = Quaternion.identity * transform.rotation * camRot;
+		transform.rotation = Quaternion.Slerp (transform.rotation,
+			playerTarg, 0.3f);
+		cam.rotation = Quaternion.Slerp (cam.rotation, 
+		                                camTarg,
+		                                0.3f);
+
+	}
+	
+	private Vector3 getMouseDelta(){
+		Vector3 delt = lastMouse - Input.mousePosition;
+		lastMouse = Input.mousePosition;
+		return delt;
+	}
+
+	void UpdatePlayerTarg(Quaternion targ){
+		playerTarg = targ;
+	}
+
+
+
+}
