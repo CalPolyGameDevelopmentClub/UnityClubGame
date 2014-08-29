c7954197f92ecb1266070c3b83cb23308f673a1d
kyle-piddington <kpiddy@gmail.com>
2014-08-28 22:34:44 -0700

    Starfish!

---
+++ Project/Assets/script/enemy/BulletOutlet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletOutlet : MonoBehaviour {
+	public GameObject bullet;
+	public float delay;
+	float timer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > delay) {
+			Instantiate (bullet,transform.position,transform.rotation);
+			timer = 0;
+		}
+		else{
+			timer+=Time.deltaTime;
+		}
+	}
+}
---
+++ Project/Assets/script/enemy/SimpleBullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleBullet : MonoBehaviour {
+	public float speed;
+	public float lifetime;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifetime -= Time.deltaTime;
+		transform.Translate (Vector3.forward * speed);
+		if (lifetime <= 0) {
+			Destroy(this.gameObject);
+		}
+	}
+}
